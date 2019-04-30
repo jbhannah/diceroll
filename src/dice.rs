04694682f50a8f6778eca80b9203ca663429c24d
Jesse B. Hannah <jesse@jbhannah.net>
2019-04-29 17:02:47 -0700

    Add dropping high/low roll

--- src/dice.rs
+++ src/dice.rs
@@ -17,16 +17,23 @@ impl fmt::Display for DiceExprError {
 }
 
 #[derive(PartialEq, Debug)]
+enum Drop {
+    DropHigh,
+    DropLow,
+}
+
+#[derive(PartialEq, Debug)]
 pub struct Dice {
     count: u16,
     sides: u16,
     modifier: i16,
+    drop: Option<Drop>,
 }
 
 impl Dice {
     pub fn new(expr: &str) -> Result<Dice, DiceExprError> {
         lazy_static! {
-            static ref RE: Regex = Regex::new(r"^(\d+)?d(\d+)([+-]\d+)?$").unwrap();
+            static ref RE: Regex = Regex::new(r"^(\d+)?d(\d+)([+-]\d+)?(-[LlHh])?$").unwrap();
         }
 
         let caps = match RE.captures(&expr) {
@@ -64,22 +71,43 @@ impl Dice {
             None => 0,
         };
 
+        let mut drop: Option<Drop> = None;
+        if count > 1 {
+            if let Some(c) = caps.get(4) {
+                drop = Some(match c.as_str().to_lowercase().as_str() {
+                    "-h" => Drop::DropHigh,
+                    "-l" => Drop::DropLow,
+                    _ => {
+                        return Err(DiceExprError {
+                            expr: expr.to_string(),
+                        })
+                    }
+                })
+            }
+        }
+
         Ok(Dice {
             count: count,
             sides: sides,
             modifier: modifier,
+            drop: drop,
         })
     }
 
     pub fn expr(&self) -> String {
         format!(
-            "{}d{}{}",
+            "{}d{}{}{}",
             self.count,
             self.sides,
             match self.modifier {
                 n if n > 0 => format!("+{}", n),
                 n if n < 0 => format!("{}", n),
                 _ => String::from(""),
+            },
+            match self.drop {
+                Some(Drop::DropHigh) => "-H",
+                Some(Drop::DropLow) => "-L",
+                None => "",
             }
         )
     }
@@ -87,7 +115,20 @@ impl Dice {
     pub fn roll(&self) -> u16 {
         let mut rng = ::rand::thread_rng();
         let roll = self.sample(&mut rng).into_iter();
-        (roll.sum::<u16>() as i16 + self.modifier) as u16
+        println!("{:?}", roll);
+
+        let sum: u16 = roll.clone().sum::<u16>()
+            - match self.drop {
+                Some(Drop::DropHigh) => roll.max().unwrap(),
+                Some(Drop::DropLow) => roll.min().unwrap(),
+                None => 0,
+            };
+
+        if -self.modifier < sum as i16 {
+            (sum as i16 + self.modifier) as u16
+        } else {
+            0
+        }
     }
 
     fn sample<R: ::rand::Rng>(&self, rng: &mut R) -> Vec<u16> {
@@ -110,7 +151,8 @@ mod tests {
             Dice {
                 count: 4,
                 sides: 4,
-                modifier: 0
+                modifier: 0,
+                drop: None,
             },
             dice
         );
@@ -118,12 +160,13 @@ mod tests {
 
     #[test]
     fn parse_modifier() {
-        let dice = Dice::new("4d4+4").unwrap();
+        let dice = Dice::new("4d4+4-H").unwrap();
         assert_eq!(
             Dice {
                 count: 4,
                 sides: 4,
-                modifier: 4
+                modifier: 4,
+                drop: Some(Drop::DropHigh),
             },
             dice
         );
@@ -139,8 +182,8 @@ mod tests {
 
     #[test]
     fn expr() {
-        let dice = Dice::new("4d4-4").unwrap();
-        assert_eq!("4d4-4", dice.expr());
+        let dice = Dice::new("4d4-4-l").unwrap();
+        assert_eq!("4d4-4-L", dice.expr());
     }
 
     #[test]
