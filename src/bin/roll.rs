04f666abf7d660d04dba55904ee5a1c408f959fb
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-06-12 12:55:35 -0700

    Upgrade Clap to v4

--- src/bin/roll.rs
+++ src/bin/roll.rs
@@ -1,31 +1,20 @@
 extern crate clap;
 extern crate diceroll;
 
-use clap::{App, Arg};
+use clap::{arg, command, ArgAction};
 use diceroll::expr::DiceExpr;
 use std::convert::TryFrom;
 
 fn main() {
-    let matches = App::new("diceroll")
-        .version("1.0")
-        .author("Jesse B. Hannah <jesse@jbhannah.net>")
-        .about("A command-line dice roller")
-        .arg(
-            Arg::with_name("EXPR")
-                .help("Dice expression(s) to roll")
-                .multiple(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .long("verbose")
-                .short('v')
-                .help("Displays details of each roll"),
-        )
-        .get_matches();
-    let verbose = matches.is_present("verbose");
+    let matches = roll().get_matches();
+    let verbose = matches.get_flag("verbose");
 
-    for expr in matches.values_of("EXPR").unwrap() {
+    for expr in matches
+        .get_many::<String>("EXPR")
+        .unwrap_or_default()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>()
+    {
         let dice = match DiceExpr::try_from(expr) {
             Ok(d) => d,
             Err(e) => {
@@ -42,4 +31,27 @@ fn main() {
             println!("Rolls: {:?} = {}\n", rolls, sum);
         }
     }
+}
+
+fn roll() -> clap::Command {
+    command!("diceroll")
+        .version("1.0")
+        .author("Jesse B. Hannah <jesse@jbhannah.net>")
+        .about("A command-line dice roller")
+        .arg(
+            arg!([EXPR] "Dice expression(s) to roll")
+                .action(ArgAction::Append)
+                .required(true),
+        )
+        .arg(
+            arg!([verbose] "Displays details of each roll")
+                .short('v')
+                .long("verbose")
+                .action(ArgAction::SetTrue),
+        )
+}
+
+#[test]
+fn verify_cli() {
+    roll().debug_assert();
 }
